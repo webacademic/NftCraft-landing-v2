585897a29f85cc145b793251c86ad0e90c65946d
Iuriy <webacademic@yandex.ru>
2022-03-31 21:46:09 +0300

    desktop slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log(1);
+import slider from './modules/slider';
+
+document.addEventListener('DOMContentLoaded', () => {
+    slider({
+        container: '.slider',
+        nextArrow: '.slider-next',
+        prevArrow: '.slider-prev',
+        slide: '.slide',
+        wrapper: '.slider-wrapper',
+        field: '.slider-inner',
+        nav: '.slider-arrows'
+    });
+})
---
+++ src/js/modules/slider.js
@@ -0,0 +1,119 @@
+function slider({container, slide, nextArrow, prevArrow, wrapper, field, nav}) {
+    let offset = 0;
+    let slideIndex = 1;
+
+    const slides = document.querySelectorAll(slide),
+        slider = document.querySelector(container),
+        prev = document.querySelector(prevArrow),
+        next = document.querySelector(nextArrow),
+        slidesWrapper = document.querySelector(wrapper),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slidesField = document.querySelector(field),
+        navs = document.querySelector(nav)
+
+    const dynamicBcg = () => {
+        slider.className = '';
+        slider.classList.add(`${container.replace(/^./,'')}`);
+        switch (+slideIndex) {
+            case 1:
+                slider.classList.add('green')
+            break
+            case 2:
+                slider.classList.add('blue')
+            break
+            case 3:
+                slider.classList.add('purple')
+            break
+            case 4:
+                slider.classList.add('orange')
+            break
+        }
+    }
+
+    slidesField.style.width = 100 * slides.length + '%';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+
+    navs.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    next.addEventListener('click', () => {
+        if (offset == (deleteNotDigits(width) * (slides.length - 1))) {
+            offset = 0;
+        } else {
+            offset += deleteNotDigits(width); 
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
+        dynamicBcg();
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = deleteNotDigits(width) * (slides.length - 1);
+        } else {
+            offset -= deleteNotDigits(width);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
+        dynamicBcg();
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = deleteNotDigits(width) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            dots.forEach(dot => dot.style.opacity = ".5");
+            dots[slideIndex-1].style.opacity = 1;
+
+            dynamicBcg();
+        });
+    });
+
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+}
+
+export default slider;
