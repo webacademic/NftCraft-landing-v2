2bd767b73c964f8a65ff8970742c21bfd930f101
Iuriy <webacademic@yandex.ru>
2022-04-01 18:56:46 +0300

    links

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-import slider from './modules/slider';
+// import slider from './modules/slider';
 
-document.addEventListener('DOMContentLoaded', () => {
-    slider({
-        container: '.slider',
-        nextArrow: '.slider-next',
-        prevArrow: '.slider-prev',
-        slide: '.slide',
-        wrapper: '.slider-wrapper',
-        field: '.slider-inner',
-        nav: '.slider-arrows'
-    });
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//     slider({
+//         container: '.slider',
+//         nextArrow: '.slider-next',
+//         prevArrow: '.slider-prev',
+//         slide: '.slide',
+//         wrapper: '.slider-wrapper',
+//         field: '.slider-inner',
+//         nav: '.slider-arrows'
+//     });
+// })
