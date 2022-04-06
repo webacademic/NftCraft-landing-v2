93e2bbbce071f42efb4e5d3800b316588e1cdd9e
Iuriy <webacademic@yandex.ru>
2022-04-06 17:11:02 +0300

    new main

---
+++ src/blocks/modules/translate/dictionary.js
@@ -0,0 +1,482 @@
+const langArr = {
+    // header
+    "menu-metaCraft":{
+        "en": "MetaCraft",
+        "ru": "MetaCraft"
+    },
+    "menu-buy-nct": {
+        "en": "Buy NCT",
+        "ru": "Купить NCT"
+    },
+    "menu-economics": {
+        "en": "Economics",
+        "ru": "Экономика"
+    },
+    "menu-achievements": {
+        "en": "Achievements",
+        "ru": "Результаты"
+    },
+    "menu-nct": {
+        "en": "Invest",
+        "ru": "Инвесторам"
+    },
+    "menu-roadmap": {
+        "en": "Roadmap",
+        "ru": "Roadmap"
+    }, 
+    //platform
+    "platform-label-1": {
+        "en": "DWARFS IN MINECRAFT!",
+        "ru": "ДВАРФЫ В MINECRAFT!"
+    },
+    "platform-title-1": {
+        "en": "PLAY AND EARN IN MINECRAFT",
+        "ru": "ИГРАЙ И ЗАРАБАТЫВАЙ <br> В MINECRAFTР"
+    },
+    "platform-descr-1": {
+        "en": "<b>Win</b> сUSD tokens or a percent of the total prize pool <br> <br> <b>Take part</b> in the construction of famous architectural structures, get rewards and become their co-owner <br> <br> Participate and win in regular tournaments with a prize pool <b>of 100 to 300 USDT</b>",
+        "ru": "<b>Выигрывай</b> токены сUSD или долю от общего призового пула <br> <br> <b>Принимай участие</b> в строительстве архитектурных известных сооружений, получай вознаграждение и становись их совладельцем <br> <br> Участвуй и побеждай в регулярных турнирах с призовым пулом <b>от 100 до 300 USDT</b>"
+    },
+    "platform-button-1": {
+        "en": "Play",
+        "ru": "Играть"
+    },
+    "platform-label-2": {
+        "en": "PLAY! MINE! INVEST!",
+        "ru": "ИГРАЙ! ДОБЫВАЙ! ИНВЕСТИРУЙ!"
+    },
+    "platform-title-2": {
+        "en": "MINING IN 2D DWARFS WORLD",
+        "ru": "МАЙНИНГ В 2D МИРЕ ДВАРФОВ"
+    },
+    "platform-descr-2": {
+        "en": "<b>Play</b> in the 2D dwarfs world and earn <b>money by mining</b> cUSD/RAD token <br> <br> <b>Minimum time</b> in the game, <b>maximum profit</b> <br> <br> <b>Make money</b> while all dirty work of mining tokens is done by your dwarfs",
+        "ru": "<b>Играй</b> в 2D мире дварфов и <b>зарабатывай на майнинге</b> токена cUSD/RAD <br> <br> <b>Минимум времени</b> в игре, <b>максимум доходности</b> <br> <br> <b>Зарабатывай</b>, пока всю грязную работу по майнингу токенов делают твои мускулистые дварфы"
+    },
+    "platform-button-2": {
+        "en": "Play",
+        "ru": "Играть"
+    },
+    "platform-label-3": {
+        "en": "UNITY",
+        "ru": "UNITY"
+    },
+    "platform-title-3": {
+        "en": "UNITY PROTOTYPE",
+        "ru": "ПРОТОТИП НА UNITY"
+    },
+    "platform-descr-3": {
+        "en": "Make money <b>fast and easy!</b> <br> <br> Your own MINECRAFT world on 3D Unity engine, accessible <b>directly from your browser</b>",
+        "ru": "Зарабатывай <b>быстро и просто!</b> <br> <br> Собственный мир MINECRAFT на 3D движке Unity доступный <b>прямо из браузера</b>"
+    },
+    "platform-button-3": {
+        "en": "Play",
+        "ru": "Играть"
+    },
+    "platform-label-4": {
+        "en": "CONNECT YOUR SERVERS TO OUR ECONOMY",
+        "ru": "ПОДКЛЮЧИТЕ ВАШИ СЕРВЕРА К НАШЕЙ ЭКОНОМИКЕ"
+    },
+    "platform-title-4": {
+        "en": "INCOME INCREASING PLATFORM FOR THE BEST GAMES",
+        "ru": "ПЛАТФОРМА ПО УВЕЛИЧЕНИЮ ДОХОДНОСТИ ЛУЧШИХ ИГР"
+    },
+    "platform-descr-4": {
+        "en": "NFTCRAFT solutions make your game an <b>effective source of income</b>. By combining game worlds with blockchain technology scaling servers and a proven economic model made it <b>even easier to earn</b>",
+        "ru": "Решения NFTCRAFT сделают вашу игру <b>эффективным источником дохода</b> за счет совмещения игровых миров с блокчейном, технологиями масштабирования серверов и <b>проверенной экономической моделью.</b>"
+    },
+    "platform-button-4": {
+        "en": "Offer",
+        "ru": "Предложение"
+    },
+    // tokenomics
+    "tokenomics-title": {
+        "en": "Tockenomic project",
+        "ru": "ТОКЕНОМИКА ПРОЕКТА"
+    },
+    "tokenomics-label": {
+        "en": "Basic concepts of Tockenomics of the Universe Metacraft",
+        "ru": "Основные понятия токеномики вселенной METACRAFT"
+    },
+    "tokenomics-name-1": {
+        "en": "Governance platform token",
+        "ru": "Управляющий токен"
+    },
+    "tokenomics-descr-1-1": {
+        "en": "Governance platform token. Meant for investment and management purposes",
+        "ru": "Управляющий (governance) токен платформы, предназначенный для инвестиционных и управленческих целей."
+    },
+    "tokenomics-name-2": {
+        "en": "CraftUSD Stable Token",
+        "ru": "Стейбл-токен CraftUSD"
+    },
+    "tokenomics-descr-2-1": {
+        "en": "cUSD consists of tokenized gold (PAXG) and Ethereum (ETH) in approximately equal shares (50/50%) at the start of the project.",
+        "ru": "Обеспечен фондом CraftFund, состоящий из токенезированного золота (PAXG) и эфира (ETH), приблизительно в равных долях (50/50%) на старте проекта."
+    },
+    "tokenomics-descr-2-2": {
+        "en": "Stabletoken cUSD will be rewarded to users, project partners and the project team.",
+        "ru": "Стейблтокеном cUSD будут вознаграждаться пользователи, партнеры проекта и команда проекта."
+    },
+    "tokenomics-name-3": {
+        "en": "Radiant token",
+        "ru": "Токен Radiant"
+    },
+    "tokenomics-descr-3-1": {
+        "en": "Token share of the total game prize pool. RAD token holders can claim % of the total prize pool at the moment of unlocking, depending on the number of tokens. The number of RAD tokens is limited (1M).",
+        "ru": "Токен-доля общего игрового фонда. Обладатели токена RAD могут претендовать на % от общего призового пула в момент раскрытия в зависимости от кол-ва токенов. Количество токенов RAD ограничено (1M)."
+    },
+    "tokenomics-descr-3-2": {
+        "en": "Up to 85% of all sales and services go to the general game fund. Thus, the value of the RAD token will grow in proportion to the liquidity in the fund.",
+        "ru": "До 85% всех продаж и услуг сервиса направляется в общий игровой фонд, таким образом, стоимость токена RAD будет расти пропорционально объему ликвидности в фонде."
+    },
+    "tokenomics-name-4": {
+        "en": "Total game fund",
+        "ru": "Общий игровой фонд"
+    },
+    "tokenomics-descr-4-1": {
+        "en": "Total game fund (CraftFund), provides all objects and mechanics of the meta-world, consists of tokenized gold (PAXG) and ether (ETH) in a 50/50% proportion. CraftFund is permanently filled with all the sales of facilities and services in the meta-world.",
+        "ru": "Обеспечивающий все объекты и механики мета-мира, состоящий из токенезированного золота (PAXG) и эфира (ETH) в пропорции 50/50%. Игровой фонд CraftFund постоянно пополняется со всех продаж объектов и услуг в мета-мире."
+    },
+    "tokenomics-descr-4-2": {
+        "en": "Also, the volume of the fund will grow in the mid-term due to an increase of number and value of supporting assets (PAXG/ETH). ",
+        "ru": "Также объем фонда в среднесрочной перспективе будет расти за счет увеличения количества и роста стоимости обеспечивающих активов (PAXG/ETH)."
+    },
+    "tokenomics-descr-5-1": {
+        "en": "The mechanisms of issue, distribution, and pricing of cUSD, RAD, and NCT will maximize encourage the creation of sustainable inner and outer value in the METACRAFT world.",
+        "ru": "Механизмы эмиссии, циркуляции и ценообразования для cUSD, RAD, NCT будут максимально поощрять создание устойчивой внутренней и внешней ценности в мире METACRAFT."
+    },
+    "tokenomics-descr-5-2": {
+        "en": "The main game fund will unlock at the price of ETH more than 15K USD. This model will allow high profits from the activity and development of game objects to almost all participants of the project.",
+        "ru": "Главный игровой фонд раскроется при цене ETH более 15K USD. Данная модель позволит получить высокую прибыль от активности и развития игровых объектов почти всем участникам проекта."
+    },
+    "tokenomics-subtitle-1": {
+        "en": "HOW PLAYERS EARN",
+        "ru": "КАК ЗАРАБАТЫВАЮТ <br> ИГРОКИ"
+    },
+    "tokenomics-token-descr-1-1": {
+        "en": "Players, by completing tasks, participating in tournaments, building architectural structures, and mining valuable resources, receive cUSD - game stable token in the amount from 1/1000 to 1/50 of the current fund size.",
+        "ru": "Игроки, выполняя задания, участвуя в турнирах, строя архитектурные сооружения и добывая ценные ресурсы, получают cUSD - игровой стейблтокен в размере от 1/1000 до 1/50 от текущего размера фонда."
+    },
+    "tokenomics-token-descr-1-2": {
+        "en": "You can withdraw cUSD over time or buy with it: RAD prize pool tokens, NCT platform tokens, NFT objects, services, or other opportunities on the platform and worlds based on it.",
+        "ru": "cUSD можно вывести в полном объеме с течением времени или купить на всю сумму на выбор: токены доли призового пула RAD, токены платформы NCT, NFT-объекты, услуги или другие возможности на платформе и мирах, созданных на ее основе."
+    },
+    "tokenomics-subtitle-2": {
+        "en": "HOW THE TEAM EARNS",
+        "ru": "КАК ЗАРАБАТЫВАЕТ <br> КОМАНДА"
+    },
+    "tokenomics-token-descr-2-1": {
+        "en": "The team earns through transaction fees of up to 15% and a share of the CraftFund's total game fund, 20%, at the time of unlock",
+        "ru": "Команда зарабатывает за счет комиссий с транзакций до 15% и доли от общего игрового фонда CraftFund в момент раскрытия в размере 20%."
+    },
+    "tokenomics-subtitle-3": {
+        "en": "HOW INVESTORS EARN MONEY",
+        "ru": "КАК ЗАРАБАТЫВАЮТ <br> ИНВЕСТОРЫ"
+    },
+    "tokenomics-token-descr-3-1": {
+        "en": "Investors can earn money by buying NCT tokens or prize pool - RAD tokens",
+        "ru": "Инвесторы могут заработать покупая токены проекта - NCT или токены призового пула - RAD"
+    },
+    "tokenomics-token-descr-3-2": {
+        "en": "The value of the control NCT token inevitably increases when the planned operational results of the project are achieved.",
+        "ru": "Стоимость управляющего токена NCT неизбежно увеличивается, при достижении планируемых операционных показателей проекта."
+    },
+    "tokenomics-token-descr-3-3": {
+        "en": "The more RAD tokens an investor bought the higher % of the total prize pool and, accordingly, the share of the reward will be received. At the unlock time of CraftFund.",
+        "ru": "Чем больше инвестор купил токенов RAD, тем больше % от общего призового пула и соответственно доля вознаграждения, в момент раскрытия CraftFund."
+    },
+    //achievements
+    "achievements-title": {
+        "en": "ACHIEVEMENTS",
+        "ru": "Результаты"
+    },
+    "achievements-name-1": {
+        "en": "Сreated prototype of METACRAFT in Minecraft world",
+        "ru": "Создали прототип метавселенной METACRAFT в мире Minecraft"
+    },
+    "achievements-link-1": {
+        "en": "Site",
+        "ru": "Сайт"
+    },
+    "achievements-name-2": {
+        "en": "Received funding of $3.7M USD",
+        "ru": "Получили финансирование в размере $3,7M USD"
+    },
+    "achievements-link-2": {
+        "en": "Proof by Request",
+        "ru": "По запросу"
+    },
+    "achievements-name-3": {
+        "en": "Regular tournaments in Minecraft 3D universe",
+        "ru": "Регулярные турниры в 3D-метавселенной Minecraft"
+    },
+    "achievements-link-3": {
+        "en": "Tournaments",
+        "ru": "Турниры"
+    },
+    "achievements-name-4": {
+        "en": "Assembled a top team of experts and developers",
+        "ru": "Собрали ТОП-команду экспертов и разработчиков"
+    },
+    "achievements-link-4": {
+        "en": "Team",
+        "ru": "Команда"
+    },
+    "achievements-name-5": {
+        "en": "Browser game on blockchain",
+        "ru": "Браузерная игра на блокчейне"
+    },
+    "achievements-link-5": {
+        "en": "Play",
+        "ru": "Игра"
+    },
+    "achievements-name-6": {
+        "en": "Sold over 1,000 NFTs and created a sustainable economy",
+        "ru": "Продали более 1.000 NFT и создали устойчивую экономику"
+    },
+    "achievements-link-6": {
+        "en": "Contract",
+        "ru": "Контракт"
+    },
+    "achievements-name-7": {
+        "en": "Multiplatform 3D game MVP on Unity",
+        "ru": "MVP мультиплатформенной 3D-игры на Unity "
+    },
+    "achievements-link-7": {
+        "en": "Play",
+        "ru": "Игра"
+    },
+    "achievements-name-8": {
+        "en": "Successfully performed presentations and mitaps, received positive feedback",
+        "ru": "Провели ряд презентаций и митапов, получили положительную оценку"
+    },
+    "achievements-link-8-1": {
+        "en": "Video 01.10.21",
+        "ru": "Видео 01.10.21"
+    },
+    "achievements-link-8-2": {
+        "en": "Video 22.02.22",
+        "ru": "Видео 22.02.22"
+    },
+    "achievements-name-9": {
+        "en": "Released of Governance-token NCT",
+        "ru": "Выпуск Governance-токена NCT"
+    },
+    "achievements-link-9": {
+        "en": "Contract",
+        "ru": "Контракт"
+    },
+    "achievements-name-10": {
+        "en": "Partnerships with large funds and strong partners",
+        "ru": "Заключили партнерства с крупными фондами и сильными партнерами"
+    },
+    "achievements-link-10": {
+        "en": "Partners",
+        "ru": "Партнеры"
+    },
+    // Roadmap
+    "road-done": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-done1": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-done2": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-done3": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-done4": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-done5": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-done6": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-done7": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-done8": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },"road-done9": {
+        "en": "DONE",
+        "ru": "ВЫПОЛНЕНО"
+    },
+    "road-progress": {
+        "en": "IN PROCESS",
+        "ru": "В ПРОЦЕССЕ"
+    },
+    "road-progress1": {
+        "en": "IN PROCESS",
+        "ru": "В ПРОЦЕССЕ"
+    },
+    "road-progress2": {
+        "en": "IN PROCESS",
+        "ru": "В ПРОЦЕССЕ"
+    },
+    "road-progress3": {
+        "en": "IN PROCESS",
+        "ru": "В ПРОЦЕССЕ"
+    },
+    "road-progress4": {
+        "en": "IN PROCESS",
+        "ru": "В ПРОЦЕССЕ"
+    },
+    "road-progress5": {
+        "en": "IN PROCESS",
+        "ru": "В ПРОЦЕССЕ"
+    },
+    
+    "road-progress6": {
+        "en": "IN PROCESS",
+        "ru": "В ПРОЦЕССЕ"
+    },
+    "road-soon": {
+        "en": "SOON",
+        "ru": "СКОРО"
+    },
+    "road-soon1": {
+        "en": "SOON",
+        "ru": "СКОРО"
+    },
+    "road-soon2": {
+        "en": "SOON",
+        "ru": "СКОРО"
+    },
+    "road-soon3": {
+        "en": "SOON",
+        "ru": "СКОРО"
+    },
+    "road-soon4": {
+        "en": "SOON",
+        "ru": "СКОРО"
+    },
+    "road-soon5": {
+        "en": "SOON",
+        "ru": "СКОРО"
+    },
+    "road-block-1": {
+        "en": "Alpha 2.0 <br> Game Web version",
+        "ru": "Alpha 2.0 <br> Web-версии игры"
+    },
+    "road-block-1-1": {
+        "en": "Game MVP",
+        "ru": "MVP-игры в web"
+    },
+    "road-block-1-2": {
+        "en": "Initial sales of NFT-dwarfs",
+        "ru": "Первичные продажи NFT-дварфов"
+    },
+    "road-block-1-3": {
+        "en": "Tournaments among players in web-version",
+        "ru": "Турниры среди игроков в web-версии"
+    },
+    "road-block-1-4": {
+        "en": "MVP with Unity gameplay",
+        "ru": "MVP с Unity-геймплеем"
+    },
+    "road-block-1-5": {
+        "en": "Alpha 2.0 <br> Game Web version",
+        "ru": "New web-version game design"
+    },
+    "road-block-2": {
+        "en": "Beta 1.0 <br> Game Web version",
+        "ru": "Beta 1.0 <br> Web-версии игры"
+    },
+    "road-block-2-1": {
+        "en": "Grand Tournament (beta) on blockchain",
+        "ru": "Гранд-турнир бета-версии на блокчейне"
+    },
+    "road-block-2-2": {
+        "en": "Bridge developing between blockchain and Minecraft",
+        "ru": "Разработка моста между блокчейном и Minecraft"
+    },
+    "road-block-2-3": {
+        "en": "Tournaments system in the world of Minecraft",
+        "ru": "Проведение турниров в мире Minecraft"
+    },
+    "road-block-3": {
+        "en": "Implementation of the target economy model and tokenometrics",
+        "ru": "Внедрение целевой модели экономики и токенометрики"
+    },
+    "road-block-3-1": {
+        "en": "META.CITY prototyping in Minecraft world",
+        "ru": "Создание прототипа META.CITY в мире Minecraft"
+    },
+    "road-block-3-2": {
+        "en": "Release of NCT/RAD/cUSD tokens",
+        "ru": "Выпуск токенов NCT/RAD/cUSD"
+    },
+    "road-block-3-3": {
+        "en": "Marketplace inside the game",
+        "ru": "Маркетплейс внутри игры"
+    },
+    "road-block-3-4": {
+        "en": "Public Roundup",
+        "ru": "Паблик-раунд"
+    },
+    "road-block-3-5": {
+        "en": "International marketing campaign launch",
+        "ru": "Запуск международной рекламной кампании"
+    },
+    "road-block-3-6": {
+        "en": "METACRAFT universe in Minecraft world launch",
+        "ru": "Запуск метавселенной METACRAFT в мире Minecraft"
+    },
+    "road-block-3-7": {
+        "en": "Major release of web-version in the Polygon blockchain",
+        "ru": "Запуск основного релиза web-версии в блокчейне Polygon"
+    },
+    "road-block-3-8": {
+        "en": "Beta-version on Unity engine with blockchain integration",
+        "ru": "Запуск beta-версии на движке Unity с интеграцией с блокчейном"
+    },
+    "road-block-3-9": {
+        "en": "Platform connection with third-party Minecraft servers",
+        "ru": "Запуск платформы для подключения сторонних Minecraft-серверов"
+    },
+    "road-block-4": {
+        "en": "Expanding METACRAFT universe up to 300K users",
+        "ru": "Расширение метавселенной METACRAFT до 300K пользователей"
+    },
+    "road-block-4-1": {
+        "en": "Creating META.CITY with all the cultural attractions of the world as NFT objects",
+        "ru": "Создание META.CITY со всеми культурными достопримечательностями мира в виде NFT-объектов"
+    },
+    "road-block-4-2": {
+        "en": "Connecting the most popular AAA games to the METACRAFT.EARTH economy",
+        "ru": "Подключение наиболее популярных AAA-игр к экономике METACRAFT.EARTH"
+    },
+    // Footer
+    "footer-about": {
+        "en": "NFTcraft.game is an innovative Play to Earn platform where the potential of NFTs are realized. ",
+        "ru": "NFTcraft.game — это инновационная платформа «Играй, чтобы заработать», где реализован потенциал NFT. "
+    },
+    "footer-partners": {
+        "en": "For partners and business inquiries contact ",
+        "ru": "Для партнеров и бизнес контактов "
+    },
+    "footer-invest": {
+        "en": "For investors ",
+        "ru": "Для инвесторов "
+    },
+    "footer-rights": {
+        "en": "All Rights Reserved",
+        "ru": "Все права защищены"
+    }   
+}
+
+export { langArr };
---
+++ src/blocks/modules/translate/translate.js
@@ -0,0 +1,76 @@
+import { langArr } from "./dictionary";
+
+let langSelect = document.querySelector("[data-lang-selector]"); 
+let allLang = ["en", "ru"];
+let defLang = "#en";
+
+const selectedLang = document.querySelector('.lang-selected'),
+    langListItems = document.querySelectorAll('.lang-list__item')
+
+let observer = new MutationObserver(mutationRecords => {
+    if (mutationRecords[0].attributeName === 'lang-value'){
+        changeUrlLang();
+    }    
+  });
+observer.observe(langSelect, {attributes:true});
+
+langListItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const target = e.target;
+        const links = item.querySelector('a');
+        const lang = window.location.hash.substring(1);
+
+        selectedLang.className = '';
+        selectedLang.classList.add(target.className)
+        selectedLang.textContent = target.textContent
+        langSelect.setAttribute("lang-value", target.getAttribute('data-lang-pick'));
+
+        if(!links.classList.contains(lang)) {
+            selectedLang.textContent = item.textContent;
+            item.remove();
+            return
+        }
+    })
+})
+
+function changeUrlLang() {
+    location.href = window.location.pathname + "#" + langSelect.getAttribute('lang-value');
+    location.reload();
+}
+
+function changeLang() {
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + defLang;
+        location.reload();
+    }
+    langSelect.value = hash;
+    for (let key in langArr) {
+        let elem = document.querySelector("[data-lang=" + key + "]");
+        if (elem && langArr[key][hash]) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+}
+
+const selectLang = () => {
+    let lang = window.location.hash.substring(1);
+    
+    selectedLang.className = '';
+    selectedLang.classList.add(lang);
+
+    langListItems.forEach(item => {
+        const links = item.querySelector('a')
+        if(links.classList.contains(lang)) {
+            selectedLang.textContent = item.textContent;
+            item.remove();
+            return
+        }
+    })
+}
+
+changeLang();
+selectLang();
+
+console.log(document.head.innerHTML);
