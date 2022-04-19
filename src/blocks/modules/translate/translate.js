d56d1ad53887621dc59aaea058e7a5bfbb626a26
Gleb Lipovchenko <gllip@mail.ru>
2022-04-19 16:56:19 +0300

    update

--- src/blocks/modules/translate/translate.js
+++ src/blocks/modules/translate/translate.js
@@ -1,58 +1,41 @@
 import { langArr } from "./dictionary";
 
-let langSelect = document.querySelectorAll("[data-lang-selector]"); 
-let allLang = ["en", "ru"];
-let defLang = "#en";
-
-const selectedLang = document.querySelectorAll('.lang-selected'),
-    langListItems = document.querySelectorAll('.lang-list__item')
-
-langListItems.forEach(item => {
-    item.addEventListener('click', (e) => {
-        changeUrlLang(item.querySelector('a'));
-    })
-})
-
-function changeUrlLang(target) {
-    location.href = window.location.pathname + "#" + target.getAttribute('data-lang-pick');
-    location.reload();
-}
-
-function changeLang() {
-    let hash = window.location.hash;
-    hash = hash.substring(1);
-    if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + defLang;
-        location.reload();
-    }
-    langSelect[0].value = hash;
-    langSelect[1].value = hash;
-    for (let key in langArr) {
-        let elem = document.querySelector("[data-lang=" + key + "]");
-        if (elem && langArr[key][hash]) {
-            elem.innerHTML = langArr[key][hash];
-        }
-    }
-}
-
-const selectLang = () => {
-    let lang = window.location.hash.substring(1);
+const selectedLang = document.querySelectorAll(".lang-selected"),
+    langListItems = document.querySelectorAll(".lang-list__item"),
+    allLang = { en: "English", ru: "Русский" },
+    defLang = "en",
+    navigatorLang = navigator.language.substring(0, 2);
     
-    selectedLang[0].className = '';
-    selectedLang[0].classList.add(lang);
-    selectedLang[1].className = '';
-    selectedLang[1].classList.add(lang);
+let currentLang = localStorage.getItem("currentLang") || navigatorLang || defLang;
 
-    langListItems.forEach(item => {
-        const links = item.querySelector('a')
-        if(links.classList.contains(lang)) {
-            selectedLang[0].textContent = item.textContent;
-            selectedLang[1].textContent = item.textContent;
-            item.remove();
-            return
-        }
-    })
+langListItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let lang = item
+      .querySelector("[data-lang-pick]")
+      .getAttribute("data-lang-pick");
+    setLangForAll(lang);
+    localStorage.setItem("currentLang", lang);
+  });
+});
+
+function updateSelectLang(lang) {
+  selectedLang.forEach((item) => {
+    item.innerHTML = getFullLang(lang);
+  });
 }
 
-changeLang();
-selectLang();
+function setLangForAll(lang) {
+  for (let key in langArr) {
+    let elem = document.querySelector("[data-lang=" + key + "]");
+    if (elem && langArr[key][lang]) {
+      elem.innerHTML = langArr[key][lang];
+    }
+  }
+  updateSelectLang(lang);
+}
+setLangForAll(currentLang);
+
+function getFullLang(lang) {
+  let fullLang = allLang[lang];
+  return fullLang;
+}
