1739d7c47c4f3f07e77ffa6162820297a355fa62
Gleb Lipovchenko <43731378+Shummik@users.noreply.github.com>
2022-04-26 15:43:53 +0300

    fix semicolumn

--- src/blocks/modules/popup-serv/popup-serv.js
+++ src/blocks/modules/popup-serv/popup-serv.js
@@ -4,11 +4,11 @@ const popupBtn = document.querySelector('.popup-btn.serv'),
 
 
 if (popupBtn) {
-    const inputs = form.querySelectorAll('input')
+    const inputs = form.querySelectorAll('input');
     popupBtn.addEventListener('click', (e) => {
         e.preventDefault();
         popup.classList.add('active');
-    })
+    });
     
     popup.addEventListener('click', (e) => {
         const target = e.target;
@@ -16,17 +16,17 @@ if (popupBtn) {
         if (target.classList.contains('popup') || target.classList.contains('popup-close')) {
             popup.classList.remove('active');
         }
-    })
+    });
     inputs.forEach(item => {
         item.addEventListener('input', () => {
     
             if (inputs[0].value !== '' && inputs[1].value !== ''  && inputs[2].value !== '') {
-                form.querySelector('button').classList.remove('inactive')
+                form.querySelector('button').classList.remove('inactive');
             } else {
-                form.querySelector('button').classList.add('inactive')
+                form.querySelector('button').classList.add('inactive');
             }
-        })
-    })
+        });
+    });
 }
 
 
